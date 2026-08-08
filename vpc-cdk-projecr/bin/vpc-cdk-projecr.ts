9ef2f8cedde2e20c0ca3bc61047f36731990480d
Fatemeh <fatemehfeizipur@gmail.com>
2026-08-08 16:35:22 -0700

    Add RDS to VPC CDK project

--- vpc-cdk-projecr/bin/vpc-cdk-projecr.ts
+++ vpc-cdk-projecr/bin/vpc-cdk-projecr.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { VpcCdkProjecrStack } from '../lib/vpc-cdk-projecr-stack';
 import { EC2Stack } from '../lib/ec2-stack';
+import { RDSStack } from '../lib/rds-stack';
 
 const app = new cdk.App();
 const vpcStack = new VpcCdkProjecrStack(app, 'VpcCdkProjecrStack', {
@@ -9,6 +10,10 @@ const vpcStack = new VpcCdkProjecrStack(app, 'VpcCdkProjecrStack', {
 
 new EC2Stack(app, 'MyEC2Stack', {
   vpc: vpcStack.vpc
-})
+});
+
+new RDSStack(app, 'RDS', {
+  vpc: vpcStack.vpc
+});
 
 app.synth()
